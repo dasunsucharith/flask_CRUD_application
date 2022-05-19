261bf8362c4dfb7fcf353fdf2a1e436745dfc714
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-19 10:47:12 +0530

    new

--- App.py
+++ App.py
@@ -13,7 +13,7 @@ class Data(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    phone = db.Column(db.String(20))
+    phone = db.Column(db.String(100))
 
     def __init__(self, name, email, phone):
         self.name = name
@@ -22,7 +22,9 @@ class Data(db.Model):
 
 @app.route('/')
 def Index():
-    return render_template("index.html")
+    all_data = Data.query.all()
+
+    return render_template("index.html", employees = all_data)
 
 @app.route('/insert', methods = ['POST'])
 def Insert():
@@ -35,7 +37,21 @@ def Insert():
         db.session.add(my_data)
         db.session.commit()
 
-        flash('Employee Inserted Successfully')
+        flash('Employee Inserted Successfully!')
+
+        return redirect(url_for('Index'))
+
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+        flash("Employee Updated Successfully!")
 
         return redirect(url_for('Index'))
 
